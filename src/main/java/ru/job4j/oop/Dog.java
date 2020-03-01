796505937df6cb5348ae0ddb5ee7be14cb3b93d1
C++ <45721225+autorobott@users.noreply.github.com>
2020-03-01 13:31:26 +0300

    1.1. Создание объекта[#245834]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    static public void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
