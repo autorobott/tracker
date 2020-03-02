178c87da698528c5ea3e8772a6483d689a907168
C++ <45721225+autorobott@users.noreply.github.com>
2020-03-02 19:18:36 +0300

    2.1. Рефакторинг - Расстояние между точками.[#245830] исправления 1.

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+    @Test
+    public void distance() {
+        Point a = new Point(1, 1);
+        Point b = new Point(2, 2);
+        double expected = 1.4142135623730951;
+        double out = a.distence(b);
+        Assert.assertEquals(expected, out, 0.001);
+    }
+
+}
