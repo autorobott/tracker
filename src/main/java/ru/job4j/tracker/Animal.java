4838fc66c5985f502f08025e6af779e3a54dab51
C++ <45721225+autorobott@users.noreply.github.com>
2020-03-02 21:25:18 +0300

    1. Конструктор по умолчанию.[#245845]

--- src/main/java/ru/job4j/tracker/Animal.java
+++ src/main/java/ru/job4j/tracker/Animal.java
@@ -1,14 +1,20 @@
 package ru.job4j.tracker;
 
+import javax.swing.*;
+
 public class Animal {
-    public Animal() {
+    private String name;
+
+    public Animal(String pName) {
+        name = pName;
         System.out.println(" ");
         System.out.println("Animal");
+        System.out.println(name);
     }
 
     public static void main(String[] args) {
-        Animal animal = new Animal();
-        Predator predator = new Predator();
-        Tiger tiger = new Tiger();
+        Animal animal = new Animal("Name 1");
+        Predator predator = new Predator("Name 2");
+        Tiger tiger = new Tiger("Name 3");
     }
 }
--- src/main/java/ru/job4j/tracker/Tiger.java
+++ src/main/java/ru/job4j/tracker/Tiger.java
@@ -1,8 +1,8 @@
 package ru.job4j.tracker;
 
 public class Tiger extends Predator {
-    public Tiger() {
-        super();
+    public Tiger(String pName) {
+        super(pName);
         System.out.println("Tiger");
     }
 }
