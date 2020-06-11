726e38cb1bc51c4d8e3dfd22bbf1b6dcf5c5e8d0
C++ <45721225+autorobott@users.noreply.github.com>
2020-06-11 16:10:10 +0300

    1. Чтение из консоли. Класс Scanner.[#245794] 1

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -8,13 +8,13 @@ public class MagicBall {
     static public void main(String[] args) {
 
         Scanner inpute = new Scanner(System.in);
-
-        while (true) {
+        boolean exit = true;
+        while (exit) {
             System.out.println("I am the great Oracle. What do you want to know? ");
             String buff = inpute.nextLine();
 
             if (buff.equals("quit")) {
-                break;
+                exit = false;
             } else {
                 String result = "May be";
                 int answer = new Random().nextInt(3);
@@ -25,6 +25,7 @@ public class MagicBall {
                     case 1:
                         result = "No";
                         break;
+                    default:
                 }
                 System.out.println(result);
             }
