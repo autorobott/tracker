1ead39ce337caa59d42858fec86547ebdef7a8e9
C++ <45721225+autorobott@users.noreply.github.com>
2020-07-01 11:30:25 +0300

    0.4. Пользовательские исключения.[#245815]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    static public int indexOf(String[] pValue, String pKey) throws ElementNotFoundException{
+        int rsl = -1;
+            for (int index = 0; index < pValue.length; index++) {
+                if(pValue[index].equals(pKey)) rsl = index;
+            }
+            if(rsl == -1) throw new ElementNotFoundException("The element is missing from the array !");
+        return rsl;
+    }
+
+    static public void main(String[] args) {
+        try {
+            indexOf(new String[] {"test", null}, "test");
+        } catch (ElementNotFoundException e) {
+
+        }
+    }
+}
