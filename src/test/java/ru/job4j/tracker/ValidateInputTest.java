8e69479bc64e5506c4237603d5a16e0741880147
C++ <45721225+autorobott@users.noreply.github.com>
2020-07-02 21:57:08 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#245810]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -23,7 +23,7 @@ public class ValidateInputTest {
                                 + "Wrong input, you can select: 0 .. 0%n"
                                 + "Menu.%n"
                                 + "0. Exit%n"
-                                + "Exit.%n"
+                                + "Exit. %n"
                 )
         ));
     }
@@ -33,7 +33,7 @@ public class ValidateInputTest {
         Output out = new StubOutput();
         Input in = new StabInput(
                 new String[] {
-                        "dsf", "0"
+                        "dsf"
         }
         );
         Tracker tracker = new Tracker();
@@ -41,5 +41,41 @@ public class ValidateInputTest {
                 new ExitAction(out)
         };
         new StartUIv1(out).init(in, tracker, actions);
+    }
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StabInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput inpute = new ValidateInput(in, out);
+        int selected = inpute.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenInvalidExit1() {
+        Output out = new StubOutput();
+        Input in = new StabInput(
+                new String[] { "asdf" , "1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] action = {
+                new ExitAction(out)
+        };
+        ValidateInput input = new ValidateInput(in, out);
+        new StartUIv1(out).init(input, tracker,action);
+        assertThat(out.toString(), is(
+                String.format(
+                                  "Menu.%n"
+                                + "0. Exit%n"
+                                + "Please enter validate data again.%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                                + "Exit.%n"
+                )
+        ));
     }
 }
