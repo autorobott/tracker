54c2c7d557402d4aa3b126db7d6aa5d740a568e8
C++ <45721225+autorobott@users.noreply.github.com>
2020-07-13 19:13:10 +0300

    1. Телефонный справочник на базе ArrayList[#245764] 1

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -6,7 +6,7 @@ public class PhoneDictionary implements PhoneDicInter{
     private ArrayList<Person> persons = new ArrayList<Person>();
 
     @Override
-    public void add(Person pPerson) {
+    public void add(Person pPerson) throws IllegalArgumentException{
         if (pPerson == null) {
             throw new NullPointerException("Key is NULL !");
         }
@@ -14,13 +14,17 @@ public class PhoneDictionary implements PhoneDicInter{
     }
 
     @Override
-    public ArrayList<Person> find(String pKey) {
+    public ArrayList<Person> find(String pKey) throws IllegalArgumentException{
         if (pKey == null) {
             throw new NullPointerException("Key is NULL !");
         }
         ArrayList<Person> personList = new ArrayList<Person>();
         for (Person value : persons) {
-            if (value.getName().contains(pKey) || value.getSurname().contains(pKey) || value.getPhone().contains(pKey) || value.getAddress().contains(pKey)) {
+            if (    value.getName().contains(pKey) ||
+                    value.getSurname().contains(pKey) ||
+                    value.getPhone().contains(pKey) ||
+                    value.getAddress().contains(pKey))
+            {
                 personList.add(value);
             }
         }
