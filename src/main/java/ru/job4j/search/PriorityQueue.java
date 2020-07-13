627d9f698ca0c28b2d3fb77b520876795ebc2c73
C++ <45721225+autorobott@users.noreply.github.com>
2020-07-13 19:25:07 +0300

    2. Очередь с приоритетом на LinkedList[#245765] 1

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -6,19 +6,11 @@ public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<Task>();
 
     public void put(Task pValue) {
-        if(tasks.size() == 0) {
-            tasks.add(pValue);
-        } else {
-            int index = 0;
-            int indexFind = pValue.getPriority();
-            for (Task value : tasks) {
-                if (value.getPriority() >= indexFind) {
-                    tasks.add(index, pValue);
-                    break;
-                }
-                index++;
-            }
+        int index = 0;
+        for (Task element : tasks) {
+            if (element.getPriority() < pValue.getPriority()) index++;
         }
+        tasks.add(index, pValue);
     }
 
     public Task take() {
@@ -29,19 +21,5 @@ public class PriorityQueue {
         for (Task value : tasks) {
             System.out.println(value.getDesc() + " : (" + value.getPriority() + ")");
         }
-    }
-
-    public static void main(String[] args) {
-        PriorityQueue pq = new PriorityQueue();
-        Task p1 = new Task("123" , 1);
-        Task p2 = new Task("123" , 3);
-        Task p3 = new Task("123" , 2);
-        Task p4 = new Task("123" , 6);
-        pq.put(p3);
-        pq.put(p3);
-        pq.put(p3);
-        pq.getAll();
-        pq.put(p1);
-        pq.getAll();
     }
 }
