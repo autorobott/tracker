4732a56f51c5966cda8e0ca0d739572070bb09ff
C++ <45721225+autorobott@users.noreply.github.com>
2020-07-15 17:10:58 +0300

    2. Идентичные тексты.[#245778]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String dublicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = dublicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String buff : origin) {
+            check.add(buff);
+        }
+        for (String buff : text) {
+            if (!check.contains(buff)) {
+                rsl = false;
+                break;
+            };
+        }
+        return rsl;
+    }
+}
