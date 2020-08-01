e717ad7e1496d0e18934a5a2574d13bc23a3e126
C++ <45721225+autorobott@users.noreply.github.com>
2020-08-01 09:48:16 +0300

    2. Комбинированный компаратор.[#245761

---
+++ src/main/java/ru/job4j/collection/Job.java
@@ -0,0 +1,54 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Job implements Comparable<Job> {
+    private String name;
+
+    private int priority;
+
+    public Job(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+                "name='" + name + '\'' +
+                ", priority=" + priority +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Job another) {
+        return Integer.compare(priority, another.priority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+        //return name.hashCode() * 13;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Job that = (Job) o;
+        return Objects.equals(name, that.getName());
+    }
+}
---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,144 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+
+    @Test
+    public void whenJobDescNameMax() {
+        List<Job> in = Arrays.asList(
+                new Job("A", 3),
+                new Job("C", 1),
+                new Job("B", 2)
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("C", 1),
+                new Job("B", 2),
+                new Job("A", 3)
+        );
+        Collections.sort(in, new JobDescByNameMax());
+        assertThat(expect, is(in));
+    }
+
+    @Test
+    public void whenJobDescNameMin() {
+        List<Job> in = Arrays.asList(
+                new Job("A", 3),
+                new Job("C", 1),
+                new Job("B", 2)
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("A", 3),
+                new Job("B", 2),
+                new Job("C", 1)
+        );
+        Collections.sort(in, new JobDescByNameMin());
+        assertThat(expect, is(in));
+    }
+
+    @Test
+    public void whenJobDescPriorityMax() {
+        List<Job> in = Arrays.asList(
+                new Job("A", 3),
+                new Job("C", 1),
+                new Job("B", 2)
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("A", 3),
+                new Job("B", 2),
+                new Job("C", 1)
+        );
+        Collections.sort(in, new JobDescByPriorityMax());
+        assertThat(expect, is(in));
+    }
+
+    @Test
+    public void whenJobDescPriorityMin() {
+        List<Job> in = Arrays.asList(
+                new Job("A", 3),
+                new Job("C", 1),
+                new Job("B", 2)
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("C", 1),
+                new Job("B", 2),
+                new Job("A", 3)
+        );
+        Collections.sort(in, new JobDescByPriorityMin());
+        assertThat(expect, is(in));
+    }
+
+    @Test
+    public void whenJobDescNameMaxPriotityMin() {
+        List<Job> in = Arrays.asList(
+                new Job("A", 3),
+                new Job("C", 1),
+                new Job("B", 2)
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("C", 1),
+                new Job("B", 2),
+                new Job("A", 3)
+        );
+        Collections.sort(in, new JobDescByNameMax().thenComparing(new JobDescByPriorityMin()));
+        assertThat(expect, is(in));
+    }
+
+    @Test
+    public void whenJobDescNameMinPriorityMax() {
+        List<Job> in = Arrays.asList(
+                new Job("A", 3),
+                new Job("C", 1),
+                new Job("B", 2)
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("A", 3),
+                new Job("B", 2),
+                new Job("C", 1)
+        );
+        Collections.sort(in, new JobDescByNameMin().thenComparing(new JobDescByPriorityMax()));
+        assertThat(expect, is(in));
+    }
+
+    @Test
+    public void whenJobDescPriorityMaxNameMin() {
+        List<Job> in = Arrays.asList(
+                new Job("A", 3),
+                new Job("C", 1),
+                new Job("B", 2)
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("A", 3),
+                new Job("B", 2),
+                new Job("C", 1)
+        );
+        Collections.sort(in, new JobDescByPriorityMax().thenComparing(new JobDescByNameMin()));
+        assertThat(expect, is(in));
+    }
+
+    @Test
+    public void whenJobDescPriorityMinNameMax() {
+        List<Job> in = Arrays.asList(
+                new Job("A", 3),
+                new Job("C", 1),
+                new Job("B", 2)
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("C", 1),
+                new Job("B", 2),
+                new Job("A", 3)
+        );
+        Collections.sort(in, new JobDescByPriorityMin().thenComparing(new JobDescByNameMax()));
+        assertThat(expect, is(in));
+    }
+}
