500048eb76fd4c321f230febb8ee8bc548fa438e
C++ <45721225+autorobott@users.noreply.github.com>
2020-08-04 12:57:15 +0300

    3. Компаратор для строк[#245758]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -4,44 +4,13 @@ import java.util.Comparator;
 
 public class StringCompare implements Comparator<String> {
     @Override
-    public int compare(String first, String second) {
+  public int compare(String left, String right) {
         int rsl = 0;
-        int firstHash = first.hashCode();
-        int secondHash = second.hashCode();
-        if (firstHash == secondHash) {
-            return rsl;
-        }
-        char[] buffFirst = first.toCharArray();
-        char[] buffSecond = second.toCharArray();
-        int firstLen = buffFirst.length;
-        int secondLen = buffSecond.length;
-        int len = firstLen - secondLen;
-        boolean len1 = false;
-        int stepMax = 0;
-        if (len <= 0) {
-            stepMax = firstLen;
-        } else {
-            stepMax = secondLen;
-            len1 = true;
-        }
-        for (int step = 0; step < stepMax; step++) {
-            int var1 = (int) buffFirst[step];
-            int var2 = (int) buffSecond[step];
-            int result = var1 - var2;
-            if (result < 0) {
-                rsl = -1;
-                break;
-            } else if (result > 0) {
-                rsl = 1;
-                break;
-            }
+        for (int step1 = 0; step1 < Math.min(left.length(), right.length()) && rsl == 0; step1++) {
+            rsl = Character.compare(left.charAt(step1), right.charAt(step1));
         }
         if (rsl == 0) {
-            if (len1) {
-                rsl = 1;
-            } else {
-                rsl = -1;
-            }
+            rsl = Integer.compare(left.length(), right.length());
         }
         return rsl;
     }
